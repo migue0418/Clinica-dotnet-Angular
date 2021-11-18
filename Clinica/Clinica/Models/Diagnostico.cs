effb8450cdb0f1be2d6bf963c86e67f11ad3d64e
migue0418 <miguealguacil@gmail.com>
2021-11-18 09:09:49 +0100

    Modelos creados

---
+++ Clinica/Clinica/Models/Diagnostico.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Clinica.Models
+{
+    public class Diagnostico
+    {
+        [ScaffoldColumn(false)]
+        public int IdDiagnostico { get; set; }
+
+        [Required]
+        public string ValoracionEspecialista { get; set; }
+
+        [Required]
+        public string Enfermedad { get; set; }
+
+        [Required]
+        public Cita Cita { get; set; }          // relacion 1 a 1
+    }
+}
